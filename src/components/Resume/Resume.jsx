ac72dc08563a305e741fbdf87b1d3d6f4d5eb95f
Piyush Goyal <piyush95881@gmail.com>
2025-07-23 09:19:57 +0530

    fixed resume routing

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,9 +1,20 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Resume = () => {
+    const navigate = useNavigate();
+
     useEffect(() => {
-        window.location.href = "https://drive.google.com/drive/folders/1vK4U_06zzNXeiPp8MF0mF8vlqBpXCotT?usp=drive_link";
-    }, []);
+        // Open resume in new tab
+        window.open(
+            "https://drive.google.com/drive/folders/1vK4U_06zzNXeiPp8MF0mF8vlqBpXCotT?usp=drive_link",
+            "_blank",
+            "noopener,noreferrer"
+        );
+
+        // Redirect this tab back to home
+        navigate("/");
+    }, [navigate]);
 
     return (
         <div style={{ color: "white", padding: "2rem" }}>
